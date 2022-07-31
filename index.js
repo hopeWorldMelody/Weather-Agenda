1078ad281988466c924c560cd1ad171aa3db135e
hopeWorldMelody <106555146+hopeWorldMelody@users.noreply.github.com>
2022-07-31 11:37:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function formatDate(date) {
 }
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = ""`;
+  let forecastHTML = "";
   forecastHTML =
     forecastHTML +
     ` <tr class="weather-forecast-date">
@@ -58,7 +58,7 @@ function displayForecast() {
               <th scope="row">☀️</th>
               
   `;
- 
+
   forecastElement.innerHTML = forecastHTML;
 }
 
